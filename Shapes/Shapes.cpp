d02074c5d4b5b5828fd6e94a8d4806e887011734
rosalie568 <rosalie568@hotmail.com>
2019-03-28 19:14:39 -0500

    Changes to main file

--- Shapes/Shapes.cpp
+++ Shapes/Shapes.cpp
@@ -1,13 +1,37 @@
-// Shapes.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Practice creating classes
 
 #include "pch.h"
+#include "rectangle.h"
+#include "Shape.h"
+#include "Square.h"
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
-}
+	//creating data values
+	int length = 0, width = 0, perimeter = 0;
 
+	cout << "Enter a length" << endl;
+	cin >> length;
+	cout << endl << "Enter a width" << endl;
+	cin >> width;
+	cout << "Width: " << width << "  Length: " << length << endl;
+
+	//check to see if length and width are the same
+	if (length == width) {
+		Square s(length);
+		perimeter = s.getPerimeter();
+		cout << endl << "Shape is square " << endl;
+		cout << "Perimeter:  " << perimeter << endl << endl;
+	}
+	else {
+
+		Rectangle r(length, width);
+		perimeter = r.getPerimeter();
+		cout << endl << "Shape is rectangle " << endl;
+		cout << "Perimeter:  " << perimeter << endl << endl;
+
+	}
+
+}
